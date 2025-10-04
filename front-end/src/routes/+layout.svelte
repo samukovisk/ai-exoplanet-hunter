11f28d6726a8ea084b239cb744e185e7809a5c8e
Bianca <biancabruna19@gmail.com>
2025-10-04 15:36:49 -0300

    Reinstala Tailwind

--- front-end/src/routes/+layout.svelte
+++ front-end/src/routes/+layout.svelte
@@ -1,28 +1,23 @@
 <script>
   import { page } from '$app/stores';
+  import "../app.css";
 </script>
 
-<div class="relative min-h-screen">
-  <!-- Vídeo de fundo -->
-  <video 
-    autoplay 
-    loop 
-    muted 
-    playsinline 
-    class="absolute top-0 left-0 -z-10 w-full h-auto md:h-full md:w-auto"
-  >
-    <source src="/7959300-hd_1920_1080_30fps.mp4" type="video/mp4" />
+<div class="video-container">
+
+  <video autoplay loop muted playsinline>
+    <source  src="/7959300-hd_1920_1080_30fps.mp4" type="video/mp4" />
   </video>
 
-  <!-- Navbar -->
-  <nav class="p-4 flex gap-4 relative z-10">
+
+  <nav class="p-4">
     <a href="/" 
        class="text-blue-600 hover:underline" 
        class:font-bold={$page.url.pathname === "/"}>
        Home
     </a>
     <a href="/baseDeDados" 
-       class="text-blue-600 hover:underline" 
+       class="text-blue-800 hover:underline" 
        class:font-bold={$page.url.pathname === "/baseDeDados"}>
        Base de Dados
     </a>
